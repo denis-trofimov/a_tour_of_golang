09ae6d26d2718883ba628ce82085843ad96a49cc
Trofimov Denis Vladimirovich <d.v.trofimov@tinkoff.ru>
2019-04-03 20:25:49 +0300

    Simplify WordCount.

--- exercise_maps.go
+++ exercise_maps.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"golang.org/x/tour/wc"
 	"strings"
+
+	"golang.org/x/tour/wc"
 )
 
 func WordCount(s string) map[string]int {
 	count := make(map[string]int)
 	substrings := strings.Fields(s)
 	for _, word := range substrings {
-		_, ok := count[word]
-		if ok {
-			count[word]++
-		} else {
-			count[word] = 1
-		}
+		count[word] += 1
 	}
 	return count
 }
