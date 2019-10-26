c91b362e333c3b2e031de780b7e787a184829cd2
Denis Trofimov <silaradost@yandex.ru>
2019-10-26 19:09:43 +0300

    feat: a second round of a tour.

---
+++ second tour round/exercise_loops_and_fuctions_newton_method.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for math.Abs(z*z-x) > 0.00000001 {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
