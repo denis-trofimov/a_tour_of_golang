3d7ee2b070113b4940a0176128c3fd8af8e31367
Denis Trofimov <silaradost@yandex.ru>
2019-10-31 01:22:19 +0300

    feat: stringer, errors.

---
+++ second tour round/exercise-errors.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", fmt.Sprint(float64(e)))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := 1.0
+	for math.Abs(z*z-x) > 0.00000001 {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
